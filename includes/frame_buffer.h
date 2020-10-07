fc74130fb6c2b82aed3ed952140e7854f8f9f44a
David Klein <davidklaindavid@gmail.com>
2020-10-08 00:47:58 +0300

    Deletes includes folder

--- includes/frame_buffer.h
+++
@@ -1,7 +0,0 @@
-#ifndef FRAME_BUFFER_H
-#define FRAME_BUFFER_H
-
-void frame_buffer_init();
-void frame_buffer_print(unsigned int x, unsigned int y, const char *s);
-
-#endif // FRAME_BUFFER_H
--- includes/mem.h
+++
@@ -1,8 +0,0 @@
-#ifndef MEM_H
-#define MEM_H
-
-void mem_init();
-void* mem_alloc();
-void mem_free(void* ptr);
-
-#endif // MEM_H
