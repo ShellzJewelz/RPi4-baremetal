c392b002ebfb278637c01609caa14d732b40a30f
David Klein <davidklaindavid@gmail.com>
2020-10-07 16:51:08 +0300

    Adds frame buffer usage to kernel

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,10 +1,15 @@
 #include "hw_config.h"
 #include "uart.h"
 #include "mem.h"
+#include "frame_buffer.h"
+
 extern "C" void kernel_main(unsigned long int cpu_id)
 {
     uart_init(115200);
     mem_init();
+    frame_buffer_init();
+
+    frame_buffer_print(80, 80, "Hello World!\n");
 
     while (true) 
     {
