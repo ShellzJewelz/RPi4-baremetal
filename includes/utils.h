d96540391706881087b48ed59e6e55f00595d096
David Klein <davidklaindavid@gmail.com>
2020-10-07 16:45:34 +0300

    Improved mem_align function

--- includes/utils.h
+++ includes/utils.h
@@ -5,9 +5,10 @@ void memcpy(void* dst, void* src, unsigned int size);
 
 unsigned int inline mem_align(unsigned int value, unsigned int alignment)
 {
-    return (value + (alignment - (value % alignment)));
+    return (value + ((alignment - 1) & ~(alignment - 1)));
 }
 
+extern "C" unsigned long int get_el();
 extern "C" void memzero(void* dst, unsigned int size);
 extern "C" void write32(void* dst, unsigned int value);
 extern "C" unsigned int read32(void* dst);
