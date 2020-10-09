63994543ce255b9323db783819c113f5baab5f96
David Klein <davidklaindavid@gmail.com>
2020-10-09 22:17:13 +0300

    Adds default parameter to uart_puti

--- include/uart.h
+++ include/uart.h
@@ -5,7 +5,7 @@ void uart_init(unsigned int baud_rate);
 void uart_putc(unsigned char c);
 char uart_getc();
 void uart_puts(const char* s);
-void uart_puti(long int number, int base);
+void uart_puti(long int number, int base=10);
 void uart_putmem(const unsigned char* src, unsigned int size, int base);
 
 #endif // UART_H
